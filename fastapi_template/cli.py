9241bda3c1df51a48a4d78c7bb658b8210a2e1a9
sumansnorkell <148213384+sumansnorkell@users.noreply.github.com>
2023-11-14 16:38:13 +0530

    Update cli.py

--- fastapi_template/cli.py
+++ fastapi_template/cli.py
@@ -36,7 +36,6 @@ def db_menu_update_info(ctx: BuilderContext, menu: SingularMenuModel) -> Builder
 
 
 def disable_orm(ctx: BuilderContext) -> Optional[MenuEntry]:
-    
     if ctx.db == "none":
         ctx.orm = "none"
         return SKIP_ENTRY
